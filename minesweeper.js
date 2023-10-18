ec4e99b80d80b41e11b236d2e37410430639085b
Xander Ward <wardxa@gmail.com>
2023-10-18 16:04:54 -0400

    really close to happy about this

--- minesweeper.js
+++ minesweeper.js
@@ -7,6 +7,71 @@ var mineLocations = [];
 var difficultyIndex = 0;
 var gazeControl = 'reveal';
 
+AFRAME.registerComponent("hide-on-hit-test-start", {
+  init: function() {
+    var el = this.el;
+    el.addEventListener("ar-hit-test-start", function() {
+      el.object3D.visible = false;
+    });
+    el.addEventListener("exit-vr", function() {
+      el.object3D.visible = true;
+    });
+  }
+});
+
+window.addEventListener("DOMContentLoaded", function() {
+  const sceneEl = document.querySelector("a-scene");
+  const message = document.getElementById("dom-overlay-message");
+
+  // If the user taps on any buttons or interactive elements we may add then prevent
+  // Any WebXR select events from firing
+  message.addEventListener("beforexrselect", e => {
+    e.preventDefault();
+  });
+
+  sceneEl.addEventListener("enter-vr", function() {
+    if (this.is("ar-mode")) {
+      // Entered AR
+      message.textContent = "";
+
+      // Hit testing is available
+      this.addEventListener(
+        "ar-hit-test-start",
+        function() {
+          message.innerHTML = `Scanning environment, finding surface.`;
+        },
+        { once: true }
+      );
+
+      // Has managed to start doing hit testing
+      this.addEventListener(
+        "ar-hit-test-achieved",
+        function() {
+          message.innerHTML = `Select the location to place<br />By tapping on the screen or selecting with your controller.`;
+        },
+        { once: true }
+      );
+
+      // User has placed an object
+      this.addEventListener(
+        "ar-hit-test-select",
+        function() {
+          // Object placed for the first time
+          message.textContent = "Well done!";
+          document.getElementById('scene').removeAttribute('ar-cursor');
+        },
+        { once: true }
+      );
+    }
+  });
+
+  sceneEl.addEventListener("exit-vr", function() {
+    message.textContent = "Exited Immersive Mode";
+    document.getElementById('scene').setAttribute('ar-cursor');
+  });
+});
+
+
 AFRAME.registerComponent('cursorchanger', {
   schema: {
     
@@ -15,7 +80,7 @@ AFRAME.registerComponent('cursorchanger', {
     let el = this.el;  // <a-box>
 
     this.xrCursor = function() {
-      el.setAttribute('cursor', 'rayOrigin: xrselect');
+      el.removeAttribute('cursor');
     };
     el.addEventListener('enter-vr', this.xrCursor, true);
 
